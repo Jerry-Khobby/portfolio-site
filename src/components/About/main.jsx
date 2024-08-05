56464b23106cf742144706bf65b71d04e9096faa
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-08-05 09:52:12 -1200

    almost through with the about section

--- src/components/About/main.jsx
+++ src/components/About/main.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const AboutComponent = () => {
   return (  
-    <div className="flex items-center justify-between flex-col ">
-      <div>
-        <h3>About Me</h3>
+    <div className="flex  items-right mx-16  flex-col ">
+      <div className='flex'>
+        <h3 className='font-bold font-serif  text-3xl dark:text-gray-200 text-slate-800'>About Me</h3>
         {/** this is the div for the heading */}</div>
       <div>{/** the real about  */}</div>
 
