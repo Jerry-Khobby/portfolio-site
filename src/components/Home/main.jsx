c8371e241f1b172db50c413d3caefaa499d39e67
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-08-09 05:06:03 -1200

    done with the site

--- src/components/Home/main.jsx
+++ src/components/Home/main.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './home.css';
 import { BsFileEarmarkPdf, BsGithub } from "react-icons/bs";
+import { Tooltip } from 'react-tooltip';
 
 export default function HomeComponent() {
   const name = "Jeremiah Anku Coblah";
-  const delay = 150;
+  const delay = 190;
   const [currentText, setCurrentText] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [infinite, setInfinite] = useState(true); // Assume you want infinite loop
+  const [infinite] = useState(true);
 
-  // use useEffect to render the things 
   useEffect(() => {
     let timeout;
 
@@ -31,11 +31,39 @@ export default function HomeComponent() {
   return (
     <div className='h-full w-full px-4 sm:px-4 lg:px-48 md:px-36 flex flex-col items-start pb-10 mt-32'>
       <div>
-        <h4 style={{ fontFamily: 'Fira Code' }} className='text-lg text-white font-semibold sm:text-2xl md:text-4xl lg:text-5xl'>
-          Hi 👋,my name is {currentText}
+        <h4 style={{ fontFamily: 'Fira Code' }} className='text-lg text-white font-normal sm:text-2xl md:text-4xl lg:text-5xl'>
+          Hi 👋, my name is {currentText}
         </h4>
+        <h4 className="text-lg text-gray-400  mt-10" style={{fontFamily:'Fira Code'}}>Software Developer</h4>
       </div>
-      {/** I want to show an icon here that people can use to view my resume or go to my github account  */}
+      <div className='mt-20'>
+        <p className='text-white text-md'style={{fontFamily:'Fira Code'}}>You can access my Resume and Github account from the link below</p>
+      </div>
+
+      <div className='flex mt-26 space-x-4'>
+        <a 
+          href="https://drive.google.com/file/d/17YFKflQxGggZ4ywoxQuwWSJmWmzgHQVN/view" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          title="Download Resume" 
+          className="bg-gray-700 p-3 rounded hover:bg-gray-800 transition"
+          data-tooltip-id="downloadResume"
+        >
+          <BsFileEarmarkPdf className="text-white text-2xl" />
+        </a>
+        <Tooltip id="downloadResume" content="Resume" />
+        <a 
+          href="https://github.com/Jerry-Khobby" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          title="GitHub" 
+          className="bg-gray-700 p-3 rounded hover:bg-gray-800 transition"
+          data-tooltip-id="Github"
+        >
+          <BsGithub className="text-white text-2xl" />
+        </a>
+        <Tooltip id="Github" content="Github" />
+      </div>
     </div>
   );
 }
