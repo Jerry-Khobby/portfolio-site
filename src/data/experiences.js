4d40cd5881145e8c0585c3873b0fde11965170d6
Jerry-Khobby <jerrymardeburg@gmail.com>
2025-03-10 10:57:40 -1200

    made some changes to my experiences section of my portfolio

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -4,44 +4,33 @@ const experience = [
     location: "Accra",
     role: "Web Development Intern",
     duration: "09/2023 - 11/2023",
-    description:"I Focused on fostering technological innovation and skills development. Developed and designed websites using HTML, CSS, and JavaScript.Built and maintained backend services with FastAPI. Collaborated with a team to deliver functional and efficient web applications."
+    description:
+      "Focused on fostering technological innovation and skills development. Developed and designed websites using HTML, CSS, and JavaScript. Built and maintained backend services with FastAPI. Collaborated with a team to deliver functional and efficient web applications.",
   },
   {
     company: "Zap Technology",
     location: "Remote",
     role: "Mobile App Developer",
     duration: "10/2023 - 12/2023",
-    description:"I Specialized in innovative mobile application development, delivering cutting-edge solutions.Developed mobile applications using React Native.Implemented state management with Redux Toolkit.Integrated Firebase for backend services and authentication."
+    description:
+      "Specialized in innovative mobile application development, delivering cutting-edge solutions. Developed mobile applications using React Native. Implemented state management with Redux Toolkit. Integrated Firebase for backend services and authentication.",
   },
   {
     company: "Xcelsz",
     location: "Remote",
     role: "Agile Web Developer",
     duration: "11/2023 - Present",
-    description:"Specializing in agile software development, delivering innovative solutions through collaborative and iterative approaches.Enhanced skills in agile development practices and methodologies.Utilized Git and GitHub for version control and collaboration.Developed frontends by interpreting Figma mockups."
-  }
-];
-
-
-
-
-/* 
-const leisure = [
-  {
-    title: "Keyboardist 🎹",
-    description: 
-    `As a skillful and dedicated keyboardist 🎹, I play for PENSA-KNUST ⛪, one of the largest and most vibrant student church bodies on campus. My role extends beyond simply playing the keyboard; I actively lead worship sessions 🙌, support the choir 🎶, and collaborate with other musicians to create a spiritually uplifting atmosphere during services.
-    I have mastered the art of playing the piano 🎼, enhancing the worship experience through dynamic and sensitive accompaniment. I am entrusted with playing during major events, including special services, conferences , and outreach programs that gather thousands of students 👥.
-    My commitment to excellence in music ministry has helped me develop strong leadership 🏅, teamwork 🤝, and improvisational skills 🎵, as I often adapt to different worship styles and lead spontaneous praise sessions. Playing for PENSA-KNUST has also deepened my understanding of music’s role in spiritual growth  and community building, making it a deeply rewarding experience both personally and for the church community at large.`
+    description:
+      "Specializing in agile software development, delivering innovative solutions through collaborative and iterative approaches. Enhanced skills in agile development practices and methodologies. Utilized Git and GitHub for version control and collaboration. Developed frontends by interpreting Figma mockups.",
   },
   {
-    title: "Member of KINGS-VINE 🎶 (PENSA-KNUST)",
-    description: 
-    `I am a proud member of KINGS-VINE 🎶, one of the most influential ministration bodies of PENSA-KNUST ⛪. KINGS-VINE is at the forefront of delivering powerful worship and praise ministrations during services, revivals, and major campus events 🎉. As part of this esteemed group, I contribute to shaping the worship experience for thousands of students , fostering an atmosphere where people can deeply connect with their faith 🙏.
-    Whether playing in intimate worship sessions  or grand campus-wide services 🏛️, being part of KINGS-VINE has allowed me to grow both musically and spiritually ✨. This platform sharpens my musical talents  and enhances my ability to minister through music, bringing joy, peace ✌️, and encouragement 💪 to the congregation. Together, we inspire and uplift the church through a shared passion for worship and service.`
-  }
-]; */
+    company: "BenchFive Labs",
+    location: "Remote",
+    role: "Backend Developer",
+    duration: "08/2024 - 01/2025",
+    description:
+      "Worked on backend development using Django and PostgreSQL. Gained experience with Next.js for frontend tasks and integrated payment systems. Utilized Ngrok for secure API tunneling, contributing to full-stack development efforts.",
+  },
+];
 
-
-
-export {experience};
+export { experience };
